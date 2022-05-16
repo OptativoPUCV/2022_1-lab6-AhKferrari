f5f25fb5daca023f28f06791231e68decef6f0cd
ahkferrari <>
2022-05-16 02:16:44 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -50,13 +50,19 @@ int is_valid(Node* n){
   for(i = 0; i < 9; i++){
     aux = calloc(10,sizeof(int));
     if(aux[n->sudo[i][j]] == 0 && n->sudo[i][j] != 0) aux[n->sudo[i][j]] = 1;
-    else return 0;  
+    else{
+      if(aux[n->sudo[i][j]] == 1 && n->sudo[i][j] != 0)
+      return 0;    
+      }
   }
 
   for(j = 0; j < 9; j++){
     aux = calloc(10,sizeof(int));
     if(aux[n->sudo[i][j]] == 0 && n->sudo[i][j] != 0) aux[n->sudo[i][j]] = 1;
-    else return 0;
+    else{ 
+      if(aux[n->sudo[i][j]] == 1 && n->sudo[i][j] != 0)
+      return 0;
+      }
   }
 
   for(k = 0; k < 9; k++){
@@ -66,8 +72,10 @@ int is_valid(Node* n){
       int j=3*(k%3) + (p%3);
     
     if(aux[n->sudo[i][j]] == 0 && n->sudo[i][j] != 0) aux[n->sudo[i][j]] = 1;
-    else return 0;  
-    
+    else {
+      if(aux[n->sudo[i][j]] == 1 && n->sudo[i][j] != 0)  
+      return 0;  
+      }
     }  
   }
   free(aux);
